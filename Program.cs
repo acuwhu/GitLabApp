6e23db22f4d82a2254e0d8038cbbdc69d6bec632
Мокроусов Иван <jdyfh531@gmail.com>
2025-04-02 11:25:29 +0300

    2 commit

--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@
         static void Main(string[] args) 
         {
             //#13
+            Console.Write("x: ");
             double x = double.Parse(Console.ReadLine());
 
             double f = Math.Sin(x) + Math.Cos(x);
+            Console.WriteLine($"f = {f}");
         }
     }
 }
